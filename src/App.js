57501cc158aa5b99920189ebbd37239c78c160fd
AxlEnr <axel.garcia0814@gmail.com>
2024-11-15 22:56:23 -0600

    First commit with all directories

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SignInSide from '../src/pages/homeLogin'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <SignInSide/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
